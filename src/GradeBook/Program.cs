1ceba61f125ac7459f02c6ecc205504b53f94231
burcakkam <budemirc@microsoft.com>
2019-10-31 12:50:32 +0100

    Added basic program and its basic test

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Burcak's Grade Book");
+
+            book.AddGrade(34.5);
+            book.AddGrade(40.5);
+            book.AddGrade(23);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The highest grade is {stats.High:n1}");
+            Console.WriteLine($"The lowest grade is {stats.Low:n1}");
+            Console.WriteLine($"The average grade is {stats.Average:n1}");
+        }
+    }
+}
